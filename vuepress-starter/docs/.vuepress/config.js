505642d2449318f540eb736f3fde7c175a5f1212
walter <swh7941@qq.com>
2020-10-28 19:45:34 +0800

    【-】删除一篇前端相关的文章

--- vuepress-starter/docs/.vuepress/config.js
+++ vuepress-starter/docs/.vuepress/config.js
@@ -88,9 +88,7 @@ module.exports = {
             collapsable: true,
             sidebarDepth: 1,
             children: [
-                {title: '了解浏览器背后的运行机制', path: '/documents/webinfo/inner-browser'},
-                {title: '前端导出Excel支持自定义单元格格式', path: '/documents/webinfo/export-excel'}
-
+                {title: '了解浏览器背后的运行机制', path: '/documents/webinfo/inner-browser'}
             ]
         }, {
             title: 'IDE开发工具',
