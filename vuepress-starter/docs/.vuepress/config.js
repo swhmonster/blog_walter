c321829017baa1b5c81deaf91e663912ed871efb
swhmonster <swh7941@qq.com>
2021-07-06 16:00:48 +0800

    【+】流量治理

--- vuepress-starter/docs/.vuepress/config.js
+++ vuepress-starter/docs/.vuepress/config.js
@@ -170,6 +170,7 @@ module.exports = {
                 {title: 'Netty-编解码技术', path: '/documents/distributedsystem/netty-serialization'},
                 {title: 'HDFS-数据存储', path: '/documents/distributedsystem/hdfs-storage'},
                 {title: '分布式事务', path: '/documents/distributedsystem/distributedtransaction'},
+                {title: '流量治理', path: '/documents/distributedsystem/trafficmanagement'},
                 {title: '透明多级分流系统-客户端缓存', path: '/documents/distributedsystem/tmds-clientcache'},
                 {title: '透明多级分流系统-域名解析', path: '/documents/distributedsystem/tmds-dns'},
                 {title: '透明多级分流系统-传输链路', path: '/documents/distributedsystem/tmds-transmissionlink'},
