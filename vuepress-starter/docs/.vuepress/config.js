08d69489c82c3ad8de6650109b83179e3d7d1146
swhmonster <swh7941@qq.com>
2021-08-19 17:15:16 +0800

    【+】linux-dhcp.md

--- vuepress-starter/docs/.vuepress/config.js
+++ vuepress-starter/docs/.vuepress/config.js
@@ -233,6 +233,7 @@ module.exports = {
             children: [
                 {title: 'Linux 目录结构', path: '/documents/operatingsystem/linux-directorystructure'},
                 {title: 'Linux 启动过程（RHEL 7）', path: '/documents/operatingsystem/linux-startprocess'},
+                {title: 'Linux 动态主机配置协议 DHCP', path: '/documents/operatingsystem/linux-dhcp'},
                 {title: '常用 Linux 命令', path: '/documents/operatingsystem/linux-command'},
                 {title: '性能分析命令', path: '/documents/operatingsystem/linux&windows-jdkanalysis'}
             ]
