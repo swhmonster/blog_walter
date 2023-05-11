e156adc95d2bcf4e6caa7646df9bc5245c74cbdf
道禹 Walter <sunwenhao.swh@alibaba-inc.com>
2023-05-11 21:13:56 +0800

    [*]公众号scan position调整

--- vuepress-starter/docs/.vuepress/theme/layouts/GlobalLayout.vue
+++ vuepress-starter/docs/.vuepress/theme/layouts/GlobalLayout.vue
@@ -4,12 +4,10 @@
         <component :is="layout"/>
         <footer>
             <img alt="wechat-scan" src="../../public/codescan8cm.jpeg" v-show="flag"
-                 style="float: right;height: 150px; width:150px; position:absolute;box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04); right: 30px;bottom: 0">
-            <div>
-                <img alt="wechat-icon" src="../../public/wechat.png"
-                     style="float: right; margin-right: 30px; margin-bottom: 20px; height: 35px;"
-                     @mouseover="changeIn()" @mouseout="changeOut()">
-            </div>
+                 style="float: right;height: 150px; width:150px; position:fixed;box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04); right: 30px;bottom: 80px">
+            <img alt="wechat-icon" src="../../public/wechat.png"
+                 style="margin-right: 30px;height: 35px;position:fixed;right: 30px;bottom: 30px;"
+                 @mouseover="changeIn()" @mouseout="changeOut()">
         </footer>
     </div>
 </template>
